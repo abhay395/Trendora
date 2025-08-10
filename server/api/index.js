d05cc0e2f5938beab32a06127074d20f7fcd9868
Abhay prajapati <abhayparja90@gmail.com>
2025-08-10 11:21:08 +0530

    Server Ready for deployment in vercel

---
+++ server/api/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import errorHandlerMiddleware from './middleware/error-handler.js'
+import cors from 'cors';
+import connectDb from './db/connectdb.js'
+import router from './routes/index.routes.js';
+import serverless from 'serverless-http';
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => res.send("Hello world"));
+app.use('/api/v1', router)
+app.use(errorHandlerMiddleware)
+connectDb()
+
+
+
+export default app
+export const handler = serverless(app)
