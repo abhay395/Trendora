41a72069e8f330d223108b1500e3d90ca7c268ab
Abhay prajapati <abhayparja90@gmail.com>
2025-08-13 13:44:10 +0530

    Update Protected Route Solve one Bug Related to page rendering

--- client/src/componente/ProtectedRoute.jsx
+++ client/src/componente/ProtectedRoute.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import useUserStore from "../store/userStore";
+import { MoonLoader } from "react-spinners";
 
 export default function ProtectedRoute({ children, allowedRole }) {
     const token = localStorage.getItem("token");
-    const { user } = useUserStore()
+    const { user, isLoading } = useUserStore();
+
+    // Wait until user state is loaded
+    if (isLoading) {
+        return <div className='h-screen flex items-center justify-center bg-gradient-to-br from-white to-gray-100'>
+            <MoonLoader color='#111' size={60} />
+        </div>
+    }
+
     // Not logged in → go to login
     if (!token) {
         return <Navigate to="/login" replace />;
     }
-    
-    // Logged in but wrong role → go to unauthorized
+
+    // Wrong role → go to unauthorized
     if (allowedRole && user?.role !== allowedRole) {
         return <Navigate to="/unauthorized" replace />;
     }
