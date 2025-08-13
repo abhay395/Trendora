a48f943651e591411039c334c321879e4877efa2
Abhay prajapati <abhayparja90@gmail.com>
2025-08-13 13:45:04 +0530

    Create Layout for admin Pages routing

---
+++ client/src/pages/Admin/AdminLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import AdminNavbar from './componente/AdminNavbar'
+import AdminSidebar from './componente/AdminSideBar'
+function AdminLayout() {
+    return (
+       <div className="flex h-screen bg-gray-50">
+      {/* Sidebar */}
+      <AdminSidebar />
+
+      {/* Main content */}
+      <div className="flex flex-col flex-1">
+        <AdminNavbar />
+        <main className="flex-1 p-4 overflow-y-auto">
+          <Outlet /> {/* All admin pages render here */}
+        </main>
+      </div>
+    </div>
+    )
+}
+
+export default AdminLayout
