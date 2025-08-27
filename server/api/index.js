622e57194567743f52011adebbb79cbd5a2cefad
Abhay prajapati <abhayparja90@gmail.com>
2025-08-27 17:17:33 +0530

    Ready for deployment

---
+++ server/api/index.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import errorHandlerMiddleware from '../middleware/error-handler.js';
+import cors from 'cors';
+import connectDb from '../db/connectdb.js';
+import router from '../routes/index.routes.js';
+import serverless from 'serverless-http';
+
+dotenv.config();
+
+const app = express();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://trendora-i8b9.vercel.app"
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true); // allow server-to-server or curl
+    const cleanedOrigin = origin.replace(/\/$/, ""); // remove trailing slash
+    if (allowedOrigins.includes(cleanedOrigin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS Not Allowed: " + origin));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
+
+app.use(express.json());
+
+app.use(async (req, res, next) => {
+  await connectDb();
+  next();
+});
+
+app.get("/", (req, res) => res.send("Hello world"));
+
+app.use('/api/v1', router);
+
+app.use(errorHandlerMiddleware);
+
+export default app;
+export const handler = serverless(app);
