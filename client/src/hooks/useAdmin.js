fbbe7ac7293352deff166fc31b86574d44eb7ff3
Abhay prajapati <abhayparja90@gmail.com>
2025-09-01 17:13:26 +0530

    Update code strucutre in admin and product hook

--- client/src/hooks/useAdmin.js
+++ client/src/hooks/useAdmin.js
@@ -1,72 +1,102 @@
-import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getordersAdminApi, createproductAdminApi, getdashBoardStaticsAdminApi, getproductAdminApi, getcategoryAdminApi, uploadBulkProductApi, softDeleteAdminProductApi, deleteAdminProductPermanently, getproductByIdAdminApi, updateProductAdminApi } from "../api/adminApi";
+import {
+  QueryClient,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import {
+  getordersAdminApi,
+  createproductAdminApi,
+  getdashBoardStaticsAdminApi,
+  getproductAdminApi,
+  getcategoryAdminApi,
+  uploadBulkProductApi,
+  softDeleteAdminProductApi,
+  deleteAdminProductPermanently,
+  getproductByIdAdminApi,
+  updateProductAdminApi,
+} from "../api/adminApi";
 import toast from "react-hot-toast";
 
-
 export const useAdminStatics = () =>
-    useQuery({ queryKey: ["admin", "dashboard-statics"], queryFn: getdashBoardStaticsAdminApi });
+  useQuery({
+    queryKey: ["admin", "dashboard-statics"],
+    queryFn: getdashBoardStaticsAdminApi,
+  });
 
 export const useAdminOrders = () => {
-    return useQuery({ queryKey: ["admin", "orders"], queryFn: getordersAdminApi });
-}
+  return useQuery({
+    queryKey: ["admin", "orders"],
+    queryFn: getordersAdminApi,
+  });
+};
 export const useAdminProducts = () => {
-    return useQuery({ queryKey: ["admin", "products"], queryFn: getproductAdminApi });
-}
+  return useQuery({
+    queryKey: ["admin", "products"],
+    queryFn: getproductAdminApi,
+  });
+};
 export const useAdminProductById = (id) => {
-    return useQuery({ queryKey: ["admin", "product", id], queryFn: () => getproductByIdAdminApi(id) });
+  return useQuery({
+    queryKey: ["admin", "product", id],
+    queryFn: () => getproductByIdAdminApi(id),
+  });
 };
 export const useCreateProduct = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: createproductAdminApi,
-        onSuccess: () => {
-            toast.success("Product created successfully");
-            queryClient.invalidateQueries(["admin", "products"]);
-        },
-    });
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: createproductAdminApi,
+    onSuccess: () => {
+      toast.success("Product created successfully");
+      queryClient.invalidateQueries(["admin", "products"]);
+    },
+  });
 };
 export const useUpdateProduct = (id) => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: (data) => updateProductAdminApi(id, data),
-        onSuccess: () => {
-            toast.success("Product updated successfully");
-            queryClient.invalidateQueries(["admin", "product", id]);
-        },
-    });
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (data) => updateProductAdminApi(id, data),
+    onSuccess: () => {
+      toast.success("Product updated successfully");
+      queryClient.invalidateQueries(["admin", "product", id]);
+    },
+  });
 };
 export const useUploadBulkProduct = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: uploadBulkProductApi,
-        onSuccess: () => {
-            toast.success("Products Uploaded successfully");
-            queryClient.invalidateQueries(["admin", "products"]);
-        },
-    });
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: uploadBulkProductApi,
+    onSuccess: () => {
+      toast.success("Products Uploaded successfully");
+      queryClient.invalidateQueries(["admin", "products"]);
+    },
+  });
 };
 export const useSoftDeleteProduct = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: softDeleteAdminProductApi,
-        onSuccess: () => {
-            toast.success("Product Soft deleted")
-            queryClient.invalidateQueries(["admin", "products"]);
-        },
-    });
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: softDeleteAdminProductApi,
+    onSuccess: () => {
+      toast.success("Product Soft deleted");
+      queryClient.invalidateQueries(["admin", "products"]);
+    },
+  });
 };
 
 export const useDeleteProductPermanently = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: deleteAdminProductPermanently,
-        onSuccess: () => {
-            toast.success("Product deleted")
-            queryClient.invalidateQueries(["admin", "products"]);
-        },
-    });
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: deleteAdminProductPermanently,
+    onSuccess: () => {
+      toast.success("Product deleted");
+      queryClient.invalidateQueries(["admin", "products"]);
+    },
+  });
 };
 
 export const useAdminCategories = () => {
-    return useQuery({ queryKey: ["admin", "categories"], queryFn: getcategoryAdminApi });
+  return useQuery({
+    queryKey: ["admin", "categories"],
+    queryFn: getcategoryAdminApi,
+  });
 };
