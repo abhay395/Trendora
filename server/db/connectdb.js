4043a43f772c93b1b3d5ef9574b12789b26c3929
Abhay prajapati <abhayparja90@gmail.com>
2025-09-30 13:26:02 +0530

    Test 18

--- server/db/connectdb.js
+++ server/db/connectdb.js
@@ -1,15 +1,38 @@
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// dotenv.config();
+// async function connectDb() {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URL, {
+//       connectTimeoutMS: 30000,
+//       socketTimeoutMS: 45000,
+//     });
+//     console.log("MongoDB connected");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// export default connectDb;
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
-async function connectDb() {
-  try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      connectTimeoutMS: 30000,
-      socketTimeoutMS: 45000,
-    });
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.log(error);
-  }
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
-export default connectDb;
+
+async function dbConnect() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    const uri = process.env.MONGODB_URL;
+    cached.promise = mongoose.connect(uri, {
+      bufferCommands: false,
+    }).then(m => m);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
