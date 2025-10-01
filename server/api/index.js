7b3f91928ec2a2d86ed030bcb89517e99662298f
Abhay prajapati <abhayparja90@gmail.com>
2025-10-01 09:32:22 +0530

    update some thing

--- server/api/index.js
+++ server/api/index.js
@@ -35,8 +35,7 @@ app.use(session({
     secure: true, // only secure in production
     sameSite: 'none',
     maxAge: 24 * 60 * 60 * 1000, // 1 day
-     domain: ".vercel.app"
-    // domain: process.env.NODE_ENV === 'production' ? '.vercel.app' : undefined
+    domain: process.env.NODE_ENV === 'production' ? '.vercel.app' : undefined
   }
 }));
 app.use(passport.initialize());
