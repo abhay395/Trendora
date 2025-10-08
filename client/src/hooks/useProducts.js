05e626c153bde353ec3919b0d3a65b5b9769fc7b
Abhay prajapati <abhayparja90@gmail.com>
2025-10-08 17:01:24 +0530

    Refactor product-related hooks to simplify API response handling by removing unnecessary data extraction, ensuring consistency across queries and mutations.

--- client/src/hooks/useProducts.js
+++ client/src/hooks/useProducts.js
@@ -16,19 +16,19 @@ import {
 export const useProducts = () =>
   useQuery({
     queryKey: ["products"],
-    queryFn: () => fetchProductApi().then((res) => res.data.result.results),
+    queryFn: () => fetchProductApi().then((res) => res.results),
     staleTime: 1000 * 60 * 10,
   });
 export const useProductsFilterd = (filter) =>
   useQuery({
     queryKey: ["filteredProduct", filter],
     queryFn: () =>
-      fetchProductApi(filter).then((res) => res.data.result.results),
+      fetchProductApi(filter).then((res) => res.results),
   });
 export const useProductById = (id) =>
   useQuery({
     queryKey: ["product", id],
-    queryFn: () => fetchProductByIdApi(id).then((res) => res.data.result),
+    queryFn: () => fetchProductByIdApi(id).then((res) => res),
     enabled: !!id, // only run if id exists
     staleTime: 1000 * 60 * 10,
   });
@@ -37,40 +37,42 @@ export const useProductReviews = (id, options = "", page) =>
   useQuery({
     queryKey: ["productReviews", id, options, page],
     queryFn: () =>
-      fetchProductReviewApi(id, options, page).then((res) => res.data.result),
+      fetchProductReviewApi(id, options, page).then((res) => res),
     enabled: !!id,
   });
 export const useFetchProductFilters = () =>
   useQuery({
     queryKey: ["filters"],
     staleTime: 1000 * 60 * 10,
-    queryFn: () => fetchProductFiltersApi().then((res) => res.data.result),
+    queryFn: () => fetchProductFiltersApi().then((res) => res),
   });
 export const useAddReview = (id) => {
   const queryClient = useQueryClient()
   return useMutation({
     mutationFn: (form) => addProductReviewApi(form),
-    onSuccess: () => {
+    onSuccess: (data) => {
+      console.log(data)
       queryClient.invalidateQueries(['product', id])
       queryClient.invalidateQueries(['productReviews', id])
     }
   });
 };
 
-export const useAddHelpfullInReview = () => {
+export const useAddHelpfullInReview = (productId, sortBy, currentPage) => {
   const queryClient = useQueryClient(); // ✅ Correct
 
   return useMutation({
     mutationFn: (reviewId) =>
-      addhelpfulInReviewApi(reviewId).then((res) => res.data.result),
+      addhelpfulInReviewApi(reviewId).then((res) => res),
 
     onSuccess: (data) => {
-      queryClient.setQueryData(["productReviews"], (old) => {
+      queryClient.setQueryData(["productReviews", productId, sortBy, currentPage], (old) => {
+        console.log(old, data)
         if (!old) return old;
 
         return {
           ...old,
-          data: old.data.map((review) =>
+          results: old.results.map((review) =>
             review._id === data._id
               ? { ...data } // <- Replace with API response
               : review
