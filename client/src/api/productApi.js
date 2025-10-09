557fd9756a8fa23e1ffd9a8b90113c166b36f8c4
Abhay prajapati <abhayparja90@gmail.com>
2025-10-09 17:30:57 +0530

    Refactor product API functions to use async/await for improved response handling, returning only the relevant data from API calls.

--- client/src/api/productApi.js
+++ client/src/api/productApi.js
@@ -2,9 +2,27 @@ import formAxios from "./instance/formAxios"
 import privateAxios from "./instance/privateAxios"
 import publicAxios from "./instance/publiAxios"
 
-export const fetchProductApi = (filter = '') => publicAxios.get(`/product?${filter}`)
-export const fetchProductFiltersApi = () => publicAxios.get('/product/filters')
-export const fetchProductByIdApi = (id) => publicAxios.get(`/product/${id}`)
-export const fetchProductReviewApi = (productId, option = '', page = 1) => publicAxios.get(`/product/${productId}/review?sortBy=${option}&limit=5&page=${page}`)
-export const addProductReviewApi = (form) => formAxios.post(`/product/review/create`, form)
-export const addhelpfulInReviewApi = (reviewId) => privateAxios.patch(`/product/review/addHelpful/${reviewId}`, {})
+export const fetchProductApi = async (filter = '') => {
+    const response = await publicAxios.get(`/product?${filter}`)
+    return response.data.result
+}
+export const fetchProductFiltersApi = async () => {
+    const response = await publicAxios.get('/product/filters')
+    return response.data.result
+}
+export const fetchProductByIdApi = async (id) => {
+    const response = await publicAxios.get(`/product/${id}`)
+    return response.data.result
+}
+export const fetchProductReviewApi = async (productId, option = '', page = 1) => {
+    const response = await publicAxios.get(`/product/${productId}/review?sortBy=${option}&limit=5&page=${page}`)
+    return response.data.result
+}
+export const addProductReviewApi = async (form) => {
+    const response = await formAxios.post(`/product/review/create`, form)
+    return response.data.result
+}
+export const addhelpfulInReviewApi = async (reviewId) => {
+    const response = await privateAxios.patch(`/product/review/addHelpful/${reviewId}`, {})
+    return response.data.result
+}
